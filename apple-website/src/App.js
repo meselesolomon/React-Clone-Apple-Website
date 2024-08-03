ba1b219488ae74a16fe532b7b7bc2da52d6094ce
meselesolomon <meselesolomon17@gmail.com>
2024-08-04 00:55:13 +0300

    added footer

--- apple-website/src/App.js
+++ apple-website/src/App.js
@@ -3,11 +3,13 @@ import './css/style.css';
 import './css/bootstrap.css';
 
 import Header from "./components/Header/Header";
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ apple-website/src/components/Footer/Footer.js
@@ -0,0 +1,202 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        
+      <footer className="footer">
+        <section className="footer-contents">
+          <div  className="footer-contents-text">
+            <p>
+            Available for Qualified Purchasers only. Qualified Purchasers receive an Apple Gift Card when they purchase an Eligible Product at a Qualifying Location through September 30, 2024. Gift card values may vary by Eligible Product. Only one Apple Gift Card per Eligible Product per Qualified Purchaser. Offer subject to availability. While supplies last. Qualified Purchasers shall receive a discount equal to the value of the Apple Gift Card off the price of the Eligible Product, but will be charged for all items in their cart, including the Apple Gift Card. Important notice regarding the checkout receipt and monthly statement for Apple Card Monthly Installments (ACMI) purchases with this promotion: Qualified Purchasers selecting ACMI (a 0% APR payment option available only in the U.S.) as payment type at checkout shall receive a discount equal to the value of the Apple Gift Card off the price of the Eligible Product. This will result in one ACMI installment plan over 12 months for the Eligible Product discounted by the instant credit, and a second ACMI installment plan over 12 months for the full price of the Apple Gift Card. The total combined amount charged over the two separate ACMI installment plans will reflect the original full retail price of the Eligible Product. Separately, Qualified Purchasers will receive and be charged for the Apple Gift Card in the amount of the applicable discount off the Eligible Product. Apple Card Monthly Installments (ACMI) is a 0% APR payment option that is only available if you select it at checkout in the U.S. for eligible products purchased at Apple Store locations, <a href="#"> apple.com</a>, the Apple Store app, or by calling 1-800-MY-APPLE, and is subject to credit approval and credit limit. See <a href="#">support.apple.com/kb/HT211204</a> for more information about eligible products. APR ranges may vary based on when you accepted an Apple Card. Cardholders who accept an Apple Card on and or after February 1, 2024: Variable APRs for Apple Card, other than ACMI, range from 19.24% to 29.49% based on creditworthiness. Rates as of February 1, 2024. Existing cardholders: See your Customer Agreement for applicable rates and fee. If you buy an ACMI-eligible product by choosing to pay in full with Apple Card (instead of using ACMI), that purchase is subject to the Apple Card variable APR, not 0% APR. Taxes and shipping on ACMI purchases are subject to the variable APR, not 0% APR. When you buy an iPhone with ACMI, you’ll need to select AT&T, T-Mobile, or Verizon as your carrier when you check out. An iPhone purchased with ACMI is always unlocked, so you can switch carriers at any time. ACMI is not available for purchases made online at the following special stores: Apple Employee Purchase Plan; participating corporate Employee Purchase Programs; Apple at Work for small businesses; Government and Veterans and Military Purchase Programs; or on refurbished devices. The last month’s payment for each product will be the product’s purchase price, less all other payments at the monthly payment amount. ACMI financing is subject to change at any time for any reason, including but not limited to installment term lengths and eligible products. See support.apple.com/kb/HT211204 for information about upcoming changes to ACMI financing. See the <a href="#">Apple Card Customer Agreement</a> for more information about ACMI financing. Apple Card is issued by Goldman Sachs Bank USA, Salt Lake City Branch. Available for qualifying applicants in the United States. If you reside in the U.S. territories, please call Goldman Sachs at 877-255-5923 with questions about accessing this offer or applying for Apple Card. This offer cannot be combined with the Apple Employee Purchase Plan or business loyalty pricing. Additional restrictions apply. View full terms and conditions of offer 
+            <a href="#">here</a>.
+         </p>
+          <p href="#">
+            1. Apple Intelligence will be available in beta on iPhone 15 Pro, iPhone 15 Pro Max, and iPad and Mac with M1 and later, with Siri and device language set to U.S. English, as part of iOS 18, iPadOS 18, and macOS Sequoia this fall.</p>
+          <p>
+            2. Trade-in values will vary based on the condition, year, and configuration of your eligible trade-in device. Not all devices are eligible for credit. You must be at least 18 years old to be eligible to trade in for credit or for an Apple Gift Card. Trade-in value may be applied toward qualifying new device purchase, or added to an Apple Gift Card. Actual value awarded is based on receipt of a qualifying device matching the description provided when estimate was made. Sales tax may be assessed on full value of a new device purchase. In-store trade-in requires presentation of a valid photo ID (local law may require saving this information). Offer may not be available in all stores, and may vary between in-store and online trade-in. Some stores may have additional requirements. Apple or its trade-in partners reserve the right to refuse or limit quantity of any trade-in transaction for any reason. More details are available from Apple’s trade-in partner for trade-in and recycling of eligible devices. Restrictions and limitations may apply.
+            </p>
+          <a href="#">Available in the U.S. on apple.com, in the Apple Store app, and at Apple Stores.</a>
+          To access and use all Apple Card features and products available only to Apple Card users, you must add Apple Card to Wallet on an iPhone or iPad that supports and has the latest version of iOS or iPadOS. Apple Card is subject to credit approval, available only for qualifying applicants in the United States, and issued by Goldman Sachs Bank USA, Salt Lake City Branch.
+          <p>If you reside in the U.S. territories, please call Goldman Sachs at 877-255-5923 with questions about Apple Card.</p>
+          <p> Learn more about how Apple Card applications are evaluated at <span><a href="#">support.apple.com/kb/HT209218</a></span>.
+         </p>
+        <p>A subscription is required for Apple TV+.</p>
+        <p> A subscription is required for Apple Arcade, Apple Fitness+, and Apple Music.</p>
+
+          </div>
+          <hr/>
+          <section className="footer-lists">
+            {/* <!-- First column  --> */}
+            <div className="footer-lists-column1">
+              <div className="footer-lists-shop">
+                <ul>
+                  <h6 className="fw-bold">Shop and Learn</h6>
+                  <li><a href="#">Mac</a></li>
+                  <li><a href="#">iPad</a></li>
+                  <li><a href="#">iPhone</a></li>
+                  <li><a href="#">Watch</a></li>
+                  <li><a href="#">Vision</a></li>
+                  <li><a href="#">AirePods</a></li>
+                  <li><a href="#">Tv & Home</a></li>
+                  <li><a href="#">AirTag</a></li>
+                  <li><a href="#">Accessories</a></li>
+                  <li><a href="#">Gift Cards</a></li>
+                </ul>
+              </div>
+        
+              <div className="apple-wallet">
+                <ul>
+                  <h6 className="fw-bold">Apple Wallet</h6>
+                  <li><a href="">Wallet</a></li>
+                  <li><a href="">Apple Card</a></li>
+                  <li><a href="">Apple Pay</a></li>
+                  <li><a href=""></a>Apple Cash</li>
+                </ul>
+            </div>
+            </div>
+
+            {/* <!-- Second column   --> */}
+            <div className="footer-lists-column2">
+              <div className="footer-lists-account">
+                <ul>
+                  <h6 className="fw-bold">Account</h6>
+                  <li><a href="#">Manage Your Apple ID</a></li>
+                  <li><a href="#">Apple Store Account</a></li>
+                  <li><a href="#">iCloud.com</a></li>
+                </ul>
+              </div>
+        
+              <div className="footer-lists-entertainment">
+                <ul>
+                  <h6 className="fw-bold">Apple One</h6>
+                  <li><a href="">Apple Tv+</a></li>
+                  <li><a href="">Apple Music</a></li>
+                  <li><a href="">Apple Fitness+</a></li>
+                  <li><a href=""></a>Apple News+</li>
+                  <li><a href="">Apple Poudcasts</a></li>
+                  <li><a href="">Apple Books+</a></li>
+                  <li><a href=""></a>Apple Store+</li>
+                </ul>
+            </div>
+            </div>
+
+            {/* !-- thired column -- */}
+            <div className="footer-lists-column3">
+              <div className="footer-lists-apple-store">
+                <ul>
+                  <h6 className="fw-bold">Find a Store</h6>
+                  <li><a href="#">Genius Bar</a></li>
+                  <li><a href="#">Today at Apple</a></li>
+                  <li><a href="#">Group Reservations</a></li>
+                  <li><a href="#">Apple Camp</a></li>
+                  <li><a href="#">Apple store App</a></li>
+                  <li><a href="#">Certified Frfurbished</a></li>
+                  <li><a href="#">Apple Trade in</a></li>
+                  <li><a href="#">Financing</a></li>
+                  <li><a href="#">Carries Deals at Apple</a></li>
+                  <li><a href="#">Shoping Help</a></li>
+                </ul>
+              </div>
+            </div>
+
+            {/* <!-- fouth column  --> */}
+            <div className="footer-lists-column4">
+              <div className="footer-lists-bussines">
+                <ul>
+                  <h6 className="fw-bold">For Business</h6>
+
+                  <li><a href="#">Apple and Business</a></li>
+                  <li><a href="#">Shope for Business</a></li>
+                </ul>
+              </div>
+        
+              <div className="footer-lists-education">
+                <ul>
+                  <h6 className="fw-bold">For Education</h6>
+
+                  <li><a href="">Apple and Education+</a></li>
+                  <li><a href="">Shop for k-12 Music</a></li>
+                  <li><a href="">Shop for College</a></li>
+                </ul>
+            </div>
+
+            <div className="footer-lists-health">
+              <ul>
+                <h6 className="fw-bold">For Healthcare</h6>
+
+                <li><a href="">Apple in Healthcare</a></li>
+                <li><a href="">Mac in Healthcare</a></li>
+                <li><a href="">Health on Apple Watch</a></li>
+                <li><a href="">Health Recored on iPhone and iPad</a></li>
+              </ul>
+            </div>
+
+          <div className="footer-lists-government">
+            <ul>
+              <h6 className="fw-bold">For Government</h6>
+
+              <li><a href="">Shop Government</a></li>
+              <li><a href="">Shop for Veterans and Military</a></li>
+            </ul>
+          </div>
+            </div>
+
+            {/* <!-- fifth column  --> */}
+            <div className="footer-lists-column5">
+              <div className="footer-lists-apple-value">
+                <ul>
+                  <h6 className="fw-bold">Apple Values</h6>
+                  <li><a href="#">Accessibility</a></li>
+                  <li><a href="#">Education</a></li>
+                  <li><a href="#">Environment</a></li>
+                  <li><a href="#">Inclusion and Diversity</a></li>
+                  <li><a href="#">Privacy</a></li>
+                  <li><a href="#">Racial Equial Equity and Justice</a></li>
+                  <li><a href="#">Supply Chain</a></li>
+                </ul>
+              </div>
+
+              <div className="footer-lists-about-apple">
+                <ul>
+                  <h6 className="">About Apple</h6>
+                  <li><a href="">Newsroom</a></li>
+                  <li><a href="">Apple Leadership</a></li>
+                  <li><a href="">Career Opportunites</a></li>
+                  <li><a href="">Investores</a></li>
+                  <li><a href="">Apple Leadership</a></li>
+                  <li><a href="">Ethics & Compliance</a></li>
+                  <li><a href="">Events</a></li>
+                  <li><a href="">Contact Apple</a></li>
+                </ul>
+            </div>
+            </div>
+          </section>
+           <hr/>
+          <div className="footer-end"> 
+            <div className="footer-end--text">
+              <p>Copyright © 2024 Apple Inc. All rights reserved. </p>
+            </div>
+            <div className="footer-end-lists">
+              <ul>
+                <li><a href="#">Provacy Policy |</a></li>
+                <li><a href="#">Terms of Use |</a></li>
+                <li><a href="#">Sales and Refunds |</a></li>
+                <li><a href="#">Legal |</a></li>
+                <li><a href="#">Site Map |</a></li>
+                
+              </ul>
+            </div>
+            <div  className="footer-end-list-right">
+              <a href="#">United States</a>
+            </div>
+          </div>
+        </section>
+      </footer>
+
+
+    </div>
+  )
+}
+
+export default Footer
