aef8dfb05b9c0efb0d87d6fdf4d7e359fb1a118c
meselesolomon <meselesolomon17@gmail.com>
2024-10-10 23:10:56 +0300

    Added Watch route

--- apple-website/src/App.js
+++ apple-website/src/App.js
@@ -9,16 +9,19 @@ import Mac from "./components/Mac";
 import Ipad from "./components/Ipad";
 import { Route, Routes } from "react-router-dom";
 import Iphone from "./components/Iphone";
+import Watch from "./components/Watch";
 
 function App() {
   return (
     <div>
       <Header />
+
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/mac" element={<Mac />} />
         <Route path="/ipad" element={<Ipad />} />
         <Route path="/iphone" element={<Iphone />} />
+        <Route path="/watch" element={<Watch />} />
       </Routes>
 
       <Footer />
---
+++ apple-website/src/components/Watch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../App.css"
+
+function Watch() {
+  return (
+    <div className='App'>
+        <h1>Watch Page</h1>
+    </div>
+  )
+}
+
+export default Watch
